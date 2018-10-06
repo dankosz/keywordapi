1a30f90b404590d0ae63a97f5fb0c17a8de27d95
Kyle Brandson <lacas.shark@gmail.com>
2018-10-06 20:33:04 +0200

    added config

--- __init__.py
+++ __init__.py
@@ -7,7 +7,14 @@ from xploreapi import XPLORE
 xplore = XPLORE("sw4kycygk4ac5vf5ces72mp8")
 
 
+## Load configuration
+con_file = open("config.json")
+config = json.load(con_file)
+con_file.close()
 
+## Initialize client
+client = ElsClient(config['apikey'])
+client.inst_token = config['insttoken']
 
 #for each article title:
     #xplore.articleTitle(title)
