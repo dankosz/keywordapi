503d7a86c4fe68c43e3b7ccf96615f8ea1206dc1
Kyle Brandson <lacas.shark@gmail.com>
2018-10-22 22:00:22 +0200

    initialize pubinf

--- mapper.py
+++ mapper.py
@@ -6,6 +6,6 @@ class MAPPER:
         pubinf = lambda: None
         for column in (row):
             print(column)
-            pubinf.column = row[column]
+            pubinf["column"] = row[column]
         print(pubinf)
         return pubinf
