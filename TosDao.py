6416ee5c05f0a5acaf8377bac5dc92dee957f9a1
Kyle Brandson <lacas.shark@gmail.com>
2018-10-23 19:07:00 +0200

    added logging

--- TosDao.py
+++ TosDao.py
@@ -34,7 +34,7 @@ class TOSDAO:
                 if bool(result_set):
                     print("keyword %s already exists in db", (keyword))
                     existingKeyword = MAPPER.mapRow(result_set[0])
-                    print(existingKeyword)
+                    print(existingKeyword.keywordcount)
                     keywordCount = existingKeyword.keywordcount + 1 
                     keywordId = existingKeyword.id
                     #if yes, increment the keywordCount
