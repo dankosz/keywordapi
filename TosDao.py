58ad86c04d5344d87a977d89d047b6f5de29127f
mac air <macair@dhcp-340.i.wlan.net.bme.hu>
2018-10-25 11:57:35 +0200

    corrected if

--- TosDao.py
+++ TosDao.py
@@ -43,21 +43,21 @@ class TOSDAO:
                     #if not, insert it
                     print("inserting keyword into keywords table: ", keyword)
                     self.dictCursor.execute("INSERT INTO keywords (keyword, keywordcount) VALUES(%s, %s)", (keyword, int(0)))
-                    #insert into the connector table
-                    insertedKeywordId = self.conn.insert_id()
-                    print("inserting into keywordpubs keywordId:", int(insertedKeywordId), ", pubinfId: ", int(pubinfId))
-                    self.dictCursor.execute("INSERT INTO keywordpubs (keywordid, id) VALUES(%s, %s)", (int(insertedKeywordId), int(pubinfId)))
-                    #refresh view (nodes)
-                    print("inserting into nodes insertedKeywordId:", int(insertedKeywordId), ", type: keyword, label: ", keyword[0:31], ", title: ", keyword)
-                    self.dictCursor.execute("INSERT INTO nodes (rowid, type, label, title) VALUES(%s, %s, %s, %s)", (int(insertedKeywordId), "keyword", keyword[0:31], keyword))
-                    keyword_node_id = self.conn.insert_id()
-                    #refresh edges
-                    self.dictCursor.execute("SELECT * FROM nodes WHERE rowid = %s AND type = %s", (pubinfId, "publ"))
-                    pub_res = self.dictCursor.fetchall()
-                    print(pub_res)
-                    publication_node = MAPPER.mapRow(pub_res[0])
-                    print("inserting into edges start: ", int(publication_node.id), ", end: ", int(keyword_node_id), ", type: desc")
-                    self.dictCursor.execute("INSERT INTO edges (start, end, type) VALUES(%s, %s, %s)", (int(publication_node.id), int(keyword_node_id), "desc"))
+                #insert into the connector table
+                insertedKeywordId = self.conn.insert_id()
+                print("inserting into keywordpubs keywordId:", int(insertedKeywordId), ", pubinfId: ", int(pubinfId))
+                self.dictCursor.execute("INSERT INTO keywordpubs (keywordid, id) VALUES(%s, %s)", (int(insertedKeywordId), int(pubinfId)))
+                #refresh view (nodes)
+                print("inserting into nodes insertedKeywordId:", int(insertedKeywordId), ", type: keyword, label: ", keyword[0:31], ", title: ", keyword)
+                self.dictCursor.execute("INSERT INTO nodes (rowid, type, label, title) VALUES(%s, %s, %s, %s)", (int(insertedKeywordId), "keyword", keyword[0:31], keyword))
+                keyword_node_id = self.conn.insert_id()
+                #refresh edges
+                self.dictCursor.execute("SELECT * FROM nodes WHERE rowid = %s AND type = %s", (pubinfId, "publ"))
+                pub_res = self.dictCursor.fetchall()
+                print(pub_res)
+                publication_node = MAPPER.mapRow(pub_res[0])
+                print("inserting into edges start: ", int(publication_node.id), ", end: ", int(keyword_node_id), ", type: desc")
+                self.dictCursor.execute("INSERT INTO edges (start, end, type) VALUES(%s, %s, %s)", (int(publication_node.id), int(keyword_node_id), "desc"))
         except:
             print("Unexpected error, rolling back:", sys.exc_info()[0])
             self.conn.rollback()
